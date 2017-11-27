e9fe96bc82748f107caafb809c7daea5152a42f6
AlexisDelorme <33450034+AlexisDelorme@users.noreply.github.com>
2017-11-27 16:23:23 +0100

    Add files via upload

---
+++ vue/menuAdmin.php
@@ -0,0 +1,12 @@
+     <div class="menu">
+    	<h1 id="h1Menu">Menu</h1>
+        <ul id="ulmenu">
+            <li class="listeMenu"><a href="panneauControleClient.php" class="lienMenu">Données Client</a></li>
+            <li class="listeMenu"><a href="gestionProfilClientt.php" class="lienMenu">Support</a></li>
+            <li class="listeMenu"><a href="gestionHabitationClient.php" class="lienMenu">Consomation</a></li>
+            <li class="listeMenu"><a href="supportClient.php" class="lienMenu">Statistique</a></li>
+  
+            
+        </ul>
+        <div class="Mdeconnexion"><a href="deconnexion.php" class="lienMenu">Se deconnecter</a></div>
+    </div>
---
+++ vue/menuClient.php
@@ -0,0 +1,12 @@
+     <div class="menu">
+    	<h1 id="h1Menu">Menu</h1>
+        <ul id="ulmenu">
+            <li class="listeMenu"><a href="panneauControleClient.php" class="lienMenu">Panneau de Controle</a></li>
+            <li class="listeMenu"><a href="gestionProfilClientt.php" class="lienMenu">Gestion du profil</a></li>
+            <li class="listeMenu"><a href="gestionHabitationClient.php" class="lienMenu">Gestion de l'habitation</a></li>
+            <li class="listeMenu"><a href="supportClient.php" class="lienMenu">Support</a></li>
+            <li class="listeMenu"><a href="consomationClient.php" class="lienMenu">Consomation</a></li>
+            
+        </ul>
+        <div class="Mdeconnexion"><a href="deconnexion.php" class="lienMenu">Se deconnecter</a></div>
+    </div>
