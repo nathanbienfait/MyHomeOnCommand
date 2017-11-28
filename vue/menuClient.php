23670d04e69af4a5000fdd44fba8d6589fb3202c
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2017-11-28 12:14:24 +0100

    Update menuClient.php

--- vue/menuClient.php
+++ vue/menuClient.php
@@ -1,13 +1,12 @@
-     <div class="menu">
-    	<h1 id="h1Menu">Menu</h1>
+<div class="menu">
+        <h1 id="h1Menu">Menu</h1>
         <ul id="ulmenu">
-            <a href="panneauControleClient.php" class="listeMenu">Panneau de Controle<li></li></a>
-            <a href="gestionProfilClientt.php" class="listeMenu">Gestion du profil<li></li></a>
+            <a href="panneauControleClient.php" class="listeMenu">Panneau de Contrôle<li></li></a>
+            <a href="gestionProfilClient.php" class="listeMenu">Gestion du profil<li></li></a>
             <a href="gestionHabitationClient.php" class="listeMenu">Gestion de l'habitation<li></li></a>
             <a href="supportClient.php" class="listeMenu">Support<li></li></a>
-            <a href="consomationClient.php" class="listeMenu">Consomation<li"></li></a>
+            <a href="consommationClient.php" class="listeMenu">Consommation<li"></li></a>
             
         </ul>
         <a href="deconnexion.php" class="Mdeconnexion">Se deconnecter</a>
-
     </div>
