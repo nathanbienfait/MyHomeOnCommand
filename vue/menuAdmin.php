21f78c26a518d5b50883fd99ab78f725797e3495
AlexisDelorme <33450034+AlexisDelorme@users.noreply.github.com>
2017-11-28 11:26:01 +0100

    Update menuAdmin.php

--- vue/menuAdmin.php
+++ vue/menuAdmin.php
@@ -1,12 +1,12 @@
      <div class="menu">
     	<h1 id="h1Menu">Menu</h1>
         <ul id="ulmenu">
-            <li class="listeMenu"><a href="panneauControleClient.php" class="lienMenu">Données Client</a></li>
-            <li class="listeMenu"><a href="gestionProfilClientt.php" class="lienMenu">Support</a></li>
-            <li class="listeMenu"><a href="gestionHabitationClient.php" class="lienMenu">Consomation</a></li>
-            <li class="listeMenu"><a href="supportClient.php" class="lienMenu">Statistique</a></li>
+            <a href="panneauControleClient.php" class="listeMenu">Données Client<li></li></a>
+            <a href="gestionProfilClientt.php" class="listeMenu">Support<li class></li></a>
+            <a href="gestionHabitationClient.php" class="listeMenu">Consomation<li></li></a>
+            <a href="supportClient.php" class="listeMenu">Statistique<li></li></a>
   
             
         </ul>
-        <div class="Mdeconnexion"><a href="deconnexion.php" class="lienMenu">Se deconnecter</a></div>
+        <a href="deconnexion.php" class="Mdeconnexion">Se deconnecter</a>
     </div>
