2854ae4f35fd7c65c0bf7e2c434a9197dd2ebf3e
nathanbienfait <nathan.bienfait78112@gmail.com>
2017-11-28 12:03:48 +0100

    Update Header.php

--- vue/Header.php
+++ vue/Header.php
@@ -1,4 +1,4 @@
 <div id="bandeau_accueil">
-        <div id="logo"><img src="images/Logoetnomsite2white.png" alt="Logo de MyHomeOnCommand" /></div>
+        <div id="logo"><img id="imagelogo" src="images/Logoetnomsite2white.png" alt="Logo de MyHomeOnCommand" /></div>
         <div id="titre">A connected home in a connected world</div>
 </div>
