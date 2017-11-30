30f71a5f5f28f5b48f17f25e446c51cd086f4ef2
nathanbienfait <nathan.bienfait78112@gmail.com>
2017-11-30 11:55:15 +0100

    Update Page_Accueil.php

--- vue/Page_Accueil.php
+++ vue/Page_Accueil.php
@@ -2,77 +2,95 @@
 <html>
     <head>
         <meta charset="utf-8"/>
+        <link rel="stylesheet" href="css/Style_General.css" />
         <link rel="stylesheet" href="css/Style_Accueil.css" />
+        <link rel="stylesheet" href="css/headerFooter.css" />
         <title>MyHomeOnCommand</title>
     </head>
     
     <body>
-        <div id="bandeau_accueil">
-            <div id="logo"><img src="images/Logoetnomsite2white.png" alt="Logo de MyHomeOnCommand" /></div>
-            <div id="titre">Bienvenue sur MyHomeOnCommand.com</div>
+        <?php include('vue/Header.php');?>
+        <div id="corps">
+        <div id="barre_gauche_accueil">
+            <div id="description_accueil">
+                <p>Qui sommes-nous?</p>
+            </div>
+            <div id="contact_accueil">
+                <p>Contact</p>
+            </div>
         </div>
-        <div id="barre_gauche"></div>
-        <div id="corps">    
+        <div id="partie_droite">    
             <div class="formulaire">
-                <form id="connexion" method="post" action="index.php">
+                <form id="connexion" method="post" action="index.php?page=panneau">
                     <p>Connexion:</p>
                     <p>
                         <label for="login">Pseudo:</label>
                         </br>
-                        <input type="text" id="login" name="login"/>
+                        <input type="text" id="login" name="login" required/>
                         </br>
                         </br>
                         <label for="mdp">Mot de passe:</label>
                         </br>
-                        <input type="text" id="mdp" name="mdp"/>
+                        <input type="password" id="mdp" name="mdp" required/>
                         </br>
                         </br>
-                        <input type="submit" value="Se Connecter"/>  
+                        <input type="submit" value="Se Connecter" name="bouton_connexion" />  
                     </p>
                 </form>
             </div>
 
             <div class="formulaire">
-                <form id="inscription" method="post" action="">
+                <form id="inscription" name="inscription" method="post" action="index.php?page=inscription">
                     <p>Formulaire d'inscription:</p>
                     <p>
                         <label for="nom_inscription">Nom:</label>
                         </br>
-                        <input type="text" id="nom_inscription" name="nom_inscription"/>
+                        <input type="text" id="nom_inscription" name="nom_inscription" required/>
                         </br>
                         </br>
                         <label for="prenom_inscription">Prénom:</label>
                         </br>
-                        <input type="text" id="prenom_inscription" name="prenom_inscription"/>
+                        <input type="text" id="prenom_inscription" name="prenom_inscription" required/>
                         </br>
                         </br>
                         <label for="telephone_inscription">Numéro de téléphone:</label>
                         </br>
-                        <input type="text" id="telephone_inscription" name="telephone_inscription"/>
+                        <input type="text" id="telephone_inscription" name="telephone_inscription" required/>
                         </br>
                         </br>
                         <label for="email_inscription">Email:</label>
                         </br>
-                        <input type="text" id="email_inscription" name="email_inscription"/>
+                        <input type="text" id="email_inscription" name="email_inscription" required/>
                         </br>
                         </br>
                         <label for="pseudo_inscription">Pseudo:</label>
                         </br>
-                        <input type="text" id="pseudo_inscription" name="pseudo_inscription"/>
+                        <input type="text" id="pseudo_inscription" name="pseudo_inscription" required/>
                         </br>
                         </br>
                         <label for="mdp_inscription">Mot de passe:</label>
                         </br>
-                        <input type="text" id="mdp_inscription" name="mdp_inscription"/>
+                        <input type="password" id="mdp_inscription" name="mdp_inscription" required/>
                         </br>
                         </br>
-                        <input type="submit" value="Se Connecter"/>  
+                        <label for="mdpconf_inscription">Confirmer le mot de passe:</label>
+                        </br>
+                        <input type="password" id="mdpconf_inscription" name="mdpconf_inscription" required/>
+                        </br>
+                        </br>
+                        <input type="submit" value="S'inscrire" name="bouton_inscription"/>  
                     </p>
                 </form>
             </div>
         
-        </div>  
-        <div id="bas">conditions générales d'utilisation</div>
+        </div>
+    </div>
+    <?php include('vue/Footer.php');?>
+    <script>
+        document.inscription.onsubmit = function()
+        {
+            return confirm('Confirmez votre inscriptions?');
+        }
+    </script>
     </body>
-    
 </html>
