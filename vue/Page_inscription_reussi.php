7912fbf199ace8e389d4218d577f0e82b7c169dd
nathanbienfait <nathan.bienfait78112@gmail.com>
2017-11-30 11:55:43 +0100

    Add files via upload

---
+++ vue/Page_inscription_reussi.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="css/Style_General.css" />
+        <link rel="stylesheet" href="css/Style_InscriptionReussi.css" />
+        <link rel="stylesheet" href="css/headerFooter.css" />
+       
+        <title>MyHomeOnCommand</title>
+    </head>
+    
+    <body>
+        <?php include('vue/Header.php');?>
+        <div id="corps">
+        
+        <p>inscription réussi</p>
+        </div>
+        <?php include('vue/Footer.php');?>
+    </body>
+</html>
