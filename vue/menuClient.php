64a82d8ecda43cb5228b5b57cd9cccb14aed0dd9
nathanbienfait <nathan.bienfait78112@gmail.com>
2017-11-30 10:26:12 +0100

    Update menuClient.php

--- vue/menuClient.php
+++ vue/menuClient.php
@@ -5,7 +5,7 @@
             <a href="gestionProfilClient.php" class="listeMenu">Gestion du profil<li></li></a>
             <a href="gestionHabitationClient.php" class="listeMenu">Gestion de l'habitation<li></li></a>
             <a href="supportClient.php" class="listeMenu">Support<li></li></a>
-            <a href="consommationClient.php" class="listeMenu">Consommation<li"></li></a>
+            <a href="consommationClient.php" class="listeMenu">Consommation<li></li></a>
             
         </ul>
         <a href="deconnexion.php" class="Mdeconnexion">Se deconnecter</a>
