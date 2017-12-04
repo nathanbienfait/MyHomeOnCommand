d2537ae1381b1aa846ee9987442443aead1721b7
Aenerya <33449933+Aenerya@users.noreply.github.com>
2017-12-04 15:11:43 +0100

    Update Footer.php

--- vue/Footer.php
+++ vue/Footer.php
@@ -1,1 +1,4 @@
-<div id="bas">conditions générales d'utilisation</div>
+<ul id="bas">
+   <li><a href="Conditionsdutilisations.php">Conditions générales d'utilisation</a></li>
+   <li><a href="Mentionslegales.php">Mentions légales</a></li>
+</ul>
