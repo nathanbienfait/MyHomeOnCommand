32b217d08c361c96c45792e5009876ff7454d51b
nathanbienfait <nathan.bienfait78112@gmail.com>
2017-12-05 11:11:48 +0100

    Update Header.php

--- vue/Header.php
+++ vue/Header.php
@@ -1,4 +1,22 @@
 <div id="bandeau_accueil">
-        <div id="logo"><img id="imagelogo" src="images/Logoetnomsite2white.png" alt="Logo de MyHomeOnCommand" /></div>
-        <div id="titre">A connected home in a connected world</div>
+    <div id="logo"><a href="index.php?page=accueil" title="Accueil"><img id="imagelogo" src="images/Logoetnomsite2white.png" alt="Logo de MyHomeOnCommand" /></a></div>
+        <div id="titre">A connected home in a connected world
+    <?php 
+        if(isset($_SESSION['prenom']))
+        {
+            if($_SESSION['type']==3)
+            {
+                $info=infoBandeau($_SESSION['id']);
+                echo "</br></br><div id='sousTitre'>Bonjour ".$info['prenom']." ". $info['nom']."</div>";
+            }
+            if($_SESSION['type']==1)
+            {
+                echo "</br></br><div id='sousTitre'>Bonjour administrateur ".$_SESSION['prenom']."</div>";
+            }
+            
+        }
+    ?>
+    </div>
+    
 </div>
+<div id='bandeau_invisible'></div>
