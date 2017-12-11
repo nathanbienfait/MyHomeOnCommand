706efa30a637aef628a01b349bd2fd78b19657d1
ElodieOkami <33449924+ElodieOkami@users.noreply.github.com>
2017-12-11 14:15:44 +0100

    Update menuClient.php

--- vue/menuClient.php
+++ vue/menuClient.php
@@ -4,7 +4,7 @@
             <a href="index.php?page=panneau" class="listeMenu">Panneau de Controle<li></li></a>
             <a href="index.php?page=gestionProfil" class="listeMenu">Gestion du profil<li></li></a>
             <a href="index.php?page=gestionHabitation" class="listeMenu">Gestion de l'habitation<li></li></a>
-            <a href="index.php?page=SupportClient" class="listeMenu">Support<li></li></a>
+            <a href="index.php?page=supportClient" class="listeMenu">Support<li></li></a>
             <a href="index.php?page=consomationClient" class="listeMenu">Consommation<li"></li></a>
             
         </ul>
