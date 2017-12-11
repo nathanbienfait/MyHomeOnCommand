dcb32e45799b6ebc696275efc1a5f4655077d680
ElodieOkami <33449924+ElodieOkami@users.noreply.github.com>
2017-12-11 14:28:48 +0100

    Update menuClient.php

--- vue/menuClient.php
+++ vue/menuClient.php
@@ -1,9 +1,9 @@
      <div class="menu">
     	<h1 id="h1Menu">Menu</h1>
         <ul id="ulmenu">
-            <a href="index.php?page=panneau" class="listeMenu">Panneau de Controle<li></li></a>
-            <a href="index.php?page=gestionProfil" class="listeMenu">Gestion du profil<li></li></a>
-            <a href="index.php?page=gestionHabitation" class="listeMenu">Gestion de l'habitation<li></li></a>
+            <a href="index.php?page=panneauControleClient" class="listeMenu">Panneau de Controle<li></li></a>
+            <a href="index.php?page=gestionProfilClient" class="listeMenu">Gestion du profil<li></li></a>
+            <a href="index.php?page=gestionHabitationClient" class="listeMenu">Gestion de l'habitation<li></li></a>
             <a href="index.php?page=supportClient" class="listeMenu">Support<li></li></a>
             <a href="index.php?page=consomationClient" class="listeMenu">Consommation<li"></li></a>
             
