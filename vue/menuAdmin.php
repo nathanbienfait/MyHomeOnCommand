cf61aefc7e1ed9549a2df22a96bec1a2030fc531
ElodieOkami <33449924+ElodieOkami@users.noreply.github.com>
2017-12-11 14:08:26 +0100

    Update menuAdmin.php

--- vue/menuAdmin.php
+++ vue/menuAdmin.php
@@ -2,7 +2,7 @@
     	<h1 id="h1Menu">Menu</h1>
         <ul id="ulmenu">
             <a href="panneauControleClient.php" class="listeMenu">Données Client<li></li></a>
-            <a href="gestionProfilClientt.php" class="listeMenu">Support<li class></li></a>
+            <a href="gestionProfilClient.php" class="listeMenu">Support<li class></li></a>
             <a href="gestionHabitationClient.php" class="listeMenu">Consomation<li></li></a>
             <a href="supportClient.php" class="listeMenu">Statistique<li></li></a>
   
