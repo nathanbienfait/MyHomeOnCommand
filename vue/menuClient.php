658b20d1d8a256676ef7fded5845524f07bacd28
AlexisDelorme <33450034+AlexisDelorme@users.noreply.github.com>
2017-12-11 14:12:30 +0100

    Update menuClient.php

--- vue/menuClient.php
+++ vue/menuClient.php
@@ -1,12 +1,13 @@
-<div class="menu">
-        <h1 id="h1Menu">Menu</h1>
+     <div class="menu">
+    	<h1 id="h1Menu">Menu</h1>
         <ul id="ulmenu">
-            <a href="panneauControleClient.php" class="listeMenu">Panneau de Contrôle<li></li></a>
-            <a href="gestionProfilClient.php" class="listeMenu">Gestion du profil<li></li></a>
-            <a href="gestionHabitationClient.php" class="listeMenu">Gestion de l'habitation<li></li></a>
-            <a href="supportClient.php" class="listeMenu">Support<li></li></a>
-            <a href="consommationClient.php" class="listeMenu">Consommation<li></li></a>
+            <a href="index.php?page=panneau" class="listeMenu">Panneau de Controle<li></li></a>
+            <a href="index.php?page=gestionProfil" class="listeMenu">Gestion du profil<li></li></a>
+            <a href="index.php?page=gestionHabitation" class="listeMenu">Gestion de l'habitation<li></li></a>
+            <a href="index.php?page=SupportClient" class="listeMenu">Support<li></li></a>
+            <a href="index.php?page=consomationClient" class="listeMenu">Consommation<li"></li></a>
             
         </ul>
-        <a href="deconnexion.php" class="Mdeconnexion">Se deconnecter</a>
+        <a href="index.php?page=accueil" class="Mdeconnexion">Se deconnecter</a>
+
     </div>
