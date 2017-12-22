6a8a3ed590f04555975d2a7560d23390bab199a9
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2017-12-22 11:55:22 +0100

    Add files via upload

---
+++ MyHomeOnCommand/vue/menuOperateur.php
@@ -0,0 +1,11 @@
+ <div class="menu">
+    	<h1 id="h1Menu">Menu</h1>
+        <ul id="ulmenu">
+            <a href="index.php?page=panneauClientOperateur" class="listeMenu">Données Client<li></li></a>
+            <a href="index.php?page=messagerieOperateur" class="listeMenu">Messagerie<li class></li></a>
+            <a href="index.php?page=statutCapteursOperateur" class="listeMenu">Statut Capteurs<li></li></a>
+  
+            
+        </ul>
+        <a href="index.php?page=accueil" class="Mdeconnexion">Se deconnecter</a>
+    </div>
