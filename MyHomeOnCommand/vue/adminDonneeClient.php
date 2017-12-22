db0bd706fa0a0f67928b00b4cb6db50282a67711
nathanbienfait <nathan.bienfait78112@gmail.com>
2017-12-22 11:53:44 +0100

    Update adminDonneeClient.php

--- MyHomeOnCommand/vue/adminDonneeClient.php
+++ MyHomeOnCommand/vue/adminDonneeClient.php
@@ -26,7 +26,6 @@
                    <th>Téléphone</th>
                    <th>Type</th>
                    <th>Pseudo</th>
-                   <th>Mot de Passe</th>
                    <th>Id du client</th>
                    
                </tr>
@@ -36,21 +35,56 @@
             $x=0;
             while($x<$taille)
             {
-                echo "  <tr>
-                        <form class='connexion' method='post' action='index.php?page=adminPanneauClient'>
-                            <td><input type='text' id='prenom' name='prenom' value='".$info[$x]."' required/></td>
-                            <td><input type='text' id='nom' name='nom' value='".$info[$x+1]."' required/></td>
-                            <td><input type='text' id='email' name='email' value='".$info[$x+2]."' required/></td>
-                            <td><input type='text' id='telephone' name='telephone' value='".$info[$x+3]."' required/></td>
-                            <td><input type='text' id='type' name='type' value='".$info[$x+4]."' required/></td>
-                            <td><input type='text' id='pseudo' name='pseudo' value='".$info[$x+5]."' required/></td>
-                            <td><input type='text' id='mdp' name='mdp' value='".$info[$x+6]."' required/></td>
-                            <td><input type='text' id='idClient' name='idClient' value='".$info[$x+7]."' required/></td>
-                            <td><input type='submit' value='Modifier' name='bouton_modifier' />  </td>
-                        </form>
-                        </tr>
+                echo " <div id='modal_".$info[$x+7]."'class='modal'>
+                          <div class='modal-content'>
+                            <span id='close_".$info[$x+7]."' >&times;</span>
+                                <form class='connexion' method='post' action='index.php?page=adminPanneauClient'>
+                                    <input type='text' id='prenom' name='prenom' value='".$info[$x]."' required/>
+                                    <input type='text' id='nom' name='nom' value='".$info[$x+1]."' required/>
+                                    <input type='text' id='email' name='email' value='".$info[$x+2]."' />
+                                    <input type='text' id='telephone' name='telephone' value='".$info[$x+3]."' />
+                                    <input type='text' id='type' name='type' value='".$info[$x+4]."' required/>
+                                    <input type='text' id='pseudo' name='pseudo' value='".$info[$x+5]."' required/>
+
+                                    <input type='text' id='idClient' name='idClient' value='".$info[$x+7]."' required/>
+                                    <input type='submit' value='Modifier' name='bouton_modifier' />
+                                </form>
+                          </div>
+                        </div>
                         ";
+                echo "  <tr>
+                            <td>".$info[$x]."</td>
+                            <td>".$info[$x+1]."</td>
+                            <td>".$info[$x+2]."</td>
+                            <td>".$info[$x+3]."</td>
+                            <td>".$info[$x+4]."</td>
+                            <td>".$info[$x+5]."</td>
+                            <td>".$info[$x+7]."</td>
+                            <td style='border: none;'><button id='".$info[$x+7]."' type='button'>Modifier</button></td>
+                        ";
+                echo "  <script>
+                            var modal".$info[$x+7]." = document.getElementById('modal_".$info[$x+7]."');
+                            var btn".$info[$x+7]." = document.getElementById('".$info[$x+7]."');
+                            var span".$info[$x+7]." = document.getElementById('close_".$info[$x+7]."');
+                                
+                                    btn".$info[$x+7].".onclick = function() {
+                                    modal".$info[$x+7].".style.display = 'block';
+
+                                }
+
+                                    span".$info[$x+7].".onclick = function() {
+                                        modal".$info[$x+7].".style.display = 'none';
+                                    }
+                                    window.onclick = function(event) {
+                                        if (event.target == modal".$info[$x+7].") {
+                                            modal".$info[$x+7].".style.display = 'none';
+                                        }
+                                    }
+
+
+                        </script>
                 
+                        ";
                 $x=$x+8;
             }
         ?>
@@ -59,6 +93,31 @@
         
         </div>
         <?php include('vue/footer.php');?>
-            
+
+<script>
+        var modal = document.getElementByClassName('modal');
+        var btn = document.getElementsByClassName('bouton_modal_modif');
+        var span = document.getElementsByClassName("close");
+        var x=0;
+        while(x<btn.length)
+            {
+                btn[x].onclick = function() {
+                modal[x].style.display = "block";
+
+                }
+                
+                span[x].onclick = function() {
+                    modal[x].style.display = "none";
+                }
+                window.onclick = function(event) {
+                    if (event.target == modal[x]) {
+                        modal[x].style.display = "none";
+                    }
+                }
+                x++;
+            }
+        
+
+</script>
     </body>
 </html>
