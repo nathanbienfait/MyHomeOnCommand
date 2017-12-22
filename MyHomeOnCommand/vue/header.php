09a0df4c0532962d998e439f26125da618834a4f
nathanbienfait <nathan.bienfait78112@gmail.com>
2017-12-22 11:56:44 +0100

    Update header.php

--- MyHomeOnCommand/vue/header.php
+++ MyHomeOnCommand/vue/header.php
@@ -1,3 +1,4 @@
+<script src="https://use.fontawesome.com/584565f215.js"></script>
 <div id="bandeau_accueil">
     <div id="logo"><a href="index.php?page=<?php if(isset($_SESSION['type'])){if($_SESSION['type']==3){echo "panneau";}if($_SESSION['type']==1){echo "adminPanneauClient";}}else{echo "accueil";} ?>" title="Accueil"><img id="imagelogo" src="images/logo.png" alt="Logo de MyHomeOnCommand" /></a></div>
         <div id="titre">A connected home in a connected world
@@ -8,6 +9,13 @@
             {
                 $info=infoBandeau($_SESSION['id']);
                 echo "</br></br><div id='sousTitre'>Bonjour ".$info['prenom']." ". $info['nom']."</div>";
+                $verif=verifInfoClient();
+                if($verif==1)
+                {
+                    $texte='Veuillez completer les informations de votre profil dans l\'onglet "Gestion du profil"';
+                    echo "<div id='annonce'><button id='Bouton'><i class='fa fa-exclamation-triangle' aria-hidden='true'>&nbsp&nbspNotification</i></button></div>";
+                }
+            
             }
             if($_SESSION['type']==1)
             {
@@ -15,8 +23,31 @@
             }
             
         }
+        
     ?>
     </div>
     
 </div>
+<div id="modalverif" class="modal">
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p><?php echo $texte; ?></p>
+  </div>
+</div>
+<script>
+        var modal = document.getElementById('modalverif');
+        var btn = document.getElementById("Bouton");
+        var span = document.getElementsByClassName("close")[0];
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+</script>
 <div id='bandeau_invisible'></div>
