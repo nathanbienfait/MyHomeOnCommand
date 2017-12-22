92f9738cb22ff1b5ec35493eb80cdee85bcd4ef0
AlexisDelorme <33450034+AlexisDelorme@users.noreply.github.com>
2017-12-22 11:43:27 +0100

    Update modeleSupport.php

--- MyHomeOnCommand/modele/modeleSupport.php
+++ MyHomeOnCommand/modele/modeleSupport.php
@@ -27,4 +27,14 @@ class QuestionReponse
         $supprime = $bdd->prepare('DELETE FROM qr WHERE id_qr=:id');
         $supprime->execute(array('id'=>$_POST['supprimer']));
     }
+    public function modifier($idqr,$contenur,$contenuq)
+    {
+        $bdd=$this->dbconnect($contenuq,$contenur,$idqr);
+        $req = $bdd->prepare('UPDATE qr SET contenu_q = :contenuq, contenu_r = :contenur WHERE id_qr = :idqr');
+        $req->execute(array(
+            'contenuq' => $contenuq,
+            'contenur' => $contenur,
+            'idqr' => $idqr
+        ));
+    }
 }
