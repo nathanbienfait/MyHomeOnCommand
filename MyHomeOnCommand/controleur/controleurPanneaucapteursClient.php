a6b22b251eff756e216ec53cc20dc3daddcb2ace
RemiBiolley <33449916+RemiBiolley@users.noreply.github.com>
2017-12-24 16:05:45 +0100

    Update controleurPanneaucapteursClient.php

--- MyHomeOnCommand/controleur/controleurPanneaucapteursClient.php
+++ MyHomeOnCommand/controleur/controleurPanneaucapteursClient.php
@@ -1,29 +1,30 @@
 <?php
 
 
-if (!isset($_GET['fonction']) || empty($_GET['fonction']))
+if (!isset($_POST['tri']) || empty($_POST['tri']))
 {
-	$fonction = 'capteurs';
+	$fonction = 'piece';
 }
 else
 {
-	$fonction=$_GET['fonction'];
+	$fonction=$_POST['tri'];
 }
 
 switch ($fonction)
 {
-	case 'capteurs' :
+	case 'piece' :
 		$vue='panneaucontroleClient';
 		$title='Vos capteurs';
 		$entete= 'Les capteurs de votre domicile' . '' . $_SESSION['prenom'];
 		$id_logements=Obtenir_id_logements($_SESSION['id']);
 		break;
 
-	case 'tri' :
-		if (isset($_POST['tri']) || $_POST['tri']=='capteurs')
-		{
-
-		}
+	case 'type_parametre' :
+		$vue='panneaucontroleClientTrie';
+		$title='Vos capteurs';
+		$entete='Les capteurs de votre domicile' . '' . $_SESSION['prenom'];
+		$idtypesEquipement=Obtenir_tous_id_type_equipement();
+		break;
 }
 
 include('vue/' . $vue . '.php');
