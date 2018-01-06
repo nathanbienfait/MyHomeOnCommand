7bfc539ad45b39986104d5a783a0a1422b2734cd
ElodieOkami <33449924+ElodieOkami@users.noreply.github.com>
2018-01-06 09:48:26 +0100

    Update header.php

--- MyHomeOnCommand/vue/header.php
+++ MyHomeOnCommand/vue/header.php
@@ -1,12 +1,23 @@
 <script src="https://use.fontawesome.com/584565f215.js"></script>
 <div id="bandeau_accueil">
     <div id="logo"><a href="index.php?page=<?php if(isset($_SESSION['type'])){if($_SESSION['type']==3){echo "panneau";}if($_SESSION['type']==1){echo "adminPanneauClient";}}else{echo "accueil";} ?>" title="Accueil"><img id="imagelogo" src="images/logo.png" alt="Logo de MyHomeOnCommand" /></a></div>
+
         <div id="titre">A connected home in a connected world
     <?php 
         if(isset($_SESSION['prenom']))
         {
             if($_SESSION['type']==3)
             {
+                
+                $bdd = new PDO('mysql:host=localhost;dbname=myhomeoncommand;charset=utf8', 'root', '');
+                $reponse=$bdd->query('SELECT contenu FROM slogan');
+                while ($donnees = $reponse->fetch())
+                {
+                    echo $donnees['contenu'];
+                }
+                $reponse->closeCursor();
+
+
                 $info=infoBandeau($_SESSION['id']);
                 echo "</br></br><div id='sousTitre'>Bonjour ".$info['prenom']." ". $info['nom']."</div>";
                 $verif=verifInfoClient();
@@ -17,9 +28,37 @@
                 }
             
             }
+
             if($_SESSION['type']==1)
             {
-                echo "</br></br><div id='sousTitre'>Bonjour administrateur ".$_SESSION['prenom']."</div>";
+              $bdd = new PDO('mysql:host=localhost;dbname=myhomeoncommand;charset=utf8', 'root', '');
+                $reponse=$bdd->query('SELECT contenu FROM slogan');
+                while ($donnees = $reponse->fetch())
+                {
+                    echo $donnees['contenu'];
+                }
+                $reponse->closeCursor();
+                ?>
+
+            <div id='basHeader'>  
+
+                <div id='bonjour'>
+                <br/><br/>
+                <?php
+                echo "Bonjour administrateur ".$_SESSION['prenom']." ";
+                ?>
+                </div>
+
+                <div id='modifier'>
+                <br/><br/>
+                <form method='POST' action='index.php?page=adminPanneauClient'>
+                    <input type="text" id="modif" name="Modifier_le_slogan" required placeholder="Modifier le slogan"/>
+                    <input type="submit" id="valider" value="Valider" name="bouton_valider" />
+                </form>
+                </div>
+
+            </div>  
+            <?php 
             }
             
         }
