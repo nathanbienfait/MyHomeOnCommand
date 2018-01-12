c5f181d85309d13e317a5c6c3d5f68863e179bbb
RemiBiolley <33449916+RemiBiolley@users.noreply.github.com>
2018-01-12 12:02:00 +0100

    Update controleurPanneaucapteursClient.php

--- MyHomeOnCommand/controleur/controleurPanneaucapteursClient.php
+++ MyHomeOnCommand/controleur/controleurPanneaucapteursClient.php
@@ -15,14 +15,14 @@ switch ($fonction)
 	case 'piece' :
 		$vue='panneaucontroleClient';
 		$title='Vos capteurs';
-		$entete= 'Les capteurs de votre domicile' . '' . $_SESSION['prenom'];
 		$id_logements=Obtenir_id_logements($_SESSION['id']);
 		break;
 
 	case 'type_parametre' :
 		$vue='panneaucontroleClientTrie';
 		$title='Vos capteurs';
-		$entete='Les capteurs de votre domicile' . '' . $_SESSION['prenom'];
+		$compteType=0;
+		$compteLogement=0;
 		$idtypesEquipement=Obtenir_tous_id_type_equipement();
 		break;
 }
