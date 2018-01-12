71fbbf7383cc14040b254f8d5e1a0916d62f9170
AlexisDelorme <33450034+AlexisDelorme@users.noreply.github.com>
2018-01-12 11:47:02 +0100

    Update modeleSupport.php

--- MyHomeOnCommand/modele/modeleSupport.php
+++ MyHomeOnCommand/modele/modeleSupport.php
@@ -17,7 +17,7 @@ class QuestionReponse
     public function recupereqr()
     {
         $db=$this->dbConnect();
-        $donneQR = $db->query('SELECT contenu_q, contenu_r, id_qr FROM qr');
+        $donneQR = $db->query('SELECT contenu_q, contenu_r, id_qr FROM qr WHERE id_type_qr = 1');
         return $donneQR;
     }
 
@@ -25,11 +25,11 @@ class QuestionReponse
     {
         $bdd=$this->dbconnect();
         $supprime = $bdd->prepare('DELETE FROM qr WHERE id_qr=:id');
-        $supprime->execute(array('id'=>$_POST['supprimer']));
+        $supprime->execute(array('id'=>$_POST['boutton_supprimer']));
     }
     public function modifier($idqr,$contenur,$contenuq)
     {
-        $bdd=$this->dbconnect($contenuq,$contenur,$idqr);
+        $bdd=$this->dbconnect();
         $req = $bdd->prepare('UPDATE qr SET contenu_q = :contenuq, contenu_r = :contenur WHERE id_qr = :idqr');
         $req->execute(array(
             'contenuq' => $contenuq,
@@ -37,4 +37,16 @@ class QuestionReponse
             'idqr' => $idqr
         ));
     }
+     public function ajouter($texteQ,$texteR,$dateQ,$dateR)
+    {
+        $bdd=$this->dbconnect();
+        $requette = $bdd->prepare('INSERT INTO qr(contenu_q,contenu_r,date_q,date_r,id_type_qr) VALUES(:texteQ, :texteR, :dateQ, :dateR, :type)');
+        $requette->execute(array(
+            'texteQ' => $texteQ,
+            'texteR' => $texteR,
+            'dateQ' => $dateQ,
+            'dateR' => $dateR,
+            'type' => 1
+        ));
+    } 
 }
