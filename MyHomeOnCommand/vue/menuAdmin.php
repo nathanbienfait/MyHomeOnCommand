45989ee6e2326f94360322f35114a3a12e40fd09
nathanbienfait <nathan.bienfait78112@gmail.com>
2018-01-15 15:29:39 +0100

    Update menuAdmin.php

--- MyHomeOnCommand/vue/menuAdmin.php
+++ MyHomeOnCommand/vue/menuAdmin.php
@@ -2,10 +2,9 @@
     	<h1 id="h1Menu">Menu</h1>
         <ul id="ulmenu">
             <a href="index.php?page=adminPanneauClient" class="listeMenu">Données Client<li></li></a>
-            <a href="index.php?page=supportAdmin" class="listeMenu">Support<li class></li></a>
+            <a href="index.php?page=supportAdmin" class="listeMenu">Support<li></li></a>
             <a href="index.php?page=consommationAdmin" class="listeMenu">Consommation<li></li></a>
-  
-            
+            <a href="index.php?page=modification" class="listeMenu">Modification<li></li></a>
         </ul>
         <a href="index.php?page=accueil" class="Mdeconnexion">Se deconnecter</a>
     </div>
