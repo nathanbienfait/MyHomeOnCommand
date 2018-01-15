1e284f2e423d7254514d2c2f9eb9237b373a29d6
nathanbienfait <nathan.bienfait78112@gmail.com>
2018-01-15 14:27:42 +0100

    Update adminDonneeClient.php

--- MyHomeOnCommand/vue/adminDonneeClient.php
+++ MyHomeOnCommand/vue/adminDonneeClient.php
@@ -37,16 +37,15 @@
             {
                 echo " <div id='modal_".$info[$x+7]."'class='modal'>
                           <div class='modal-content'>
-                            <span id='close_".$info[$x+7]."' >&times;</span>
+                            <span class='close' id='close_".$info[$x+7]."' >&times;</span>
                                 <form class='connexion' method='post' action='index.php?page=adminPanneauClient'>
-                                    <input type='text' id='prenom' name='prenom' value='".$info[$x]."' required/>
-                                    <input type='text' id='nom' name='nom' value='".$info[$x+1]."' required/>
-                                    <input type='text' id='email' name='email' value='".$info[$x+2]."' />
-                                    <input type='text' id='telephone' name='telephone' value='".$info[$x+3]."' />
-                                    <input type='text' id='type' name='type' value='".$info[$x+4]."' required/>
-                                    <input type='text' id='pseudo' name='pseudo' value='".$info[$x+5]."' required/>
-
-                                    <input type='text' id='idClient' name='idClient' value='".$info[$x+7]."' required/>
+                                    <label for='prenom'>Prénom</label><br><input type='text' id='prenom' name='prenom' value='".$info[$x]."' required/><br><br>
+                                    <label for='nom'>Nom</label><br><input type='text' id='nom' name='nom' value='".$info[$x+1]."' required/><br><br>
+                                    <label for='email'>Email</label><br><input type='text' id='email' name='email' value='".$info[$x+2]."' /><br><br>
+                                    <label for='telephone'>Téléphone</label><br><input type='text' id='telephone' name='telephone' value='".$info[$x+3]."' /><br><br>
+                                    <label for='type'>Type</label><br><input type='text' id='type' name='type' value='".$info[$x+4]."' required/><br><br>
+                                    <label for='pseudo'>Identifiant</label><br><input type='text' id='pseudo' name='pseudo' value='".$info[$x+5]."' required/><br><br>
+                                    <label for='idClient'>Id Client</label><br><input type='text' id='idClient' name='idClient' value='".$info[$x+7]."' required/><br><br>
                                     <input type='submit' value='Modifier' name='bouton_modifier' />
                                 </form>
                           </div>
@@ -60,64 +59,40 @@
                             <td>".$info[$x+4]."</td>
                             <td>".$info[$x+5]."</td>
                             <td>".$info[$x+7]."</td>
-                            <td style='border: none;'><button id='".$info[$x+7]."' type='button'>Modifier</button></td>
-                        ";
-                echo "  <script>
-                            var modal".$info[$x+7]." = document.getElementById('modal_".$info[$x+7]."');
-                            var btn".$info[$x+7]." = document.getElementById('".$info[$x+7]."');
-                            var span".$info[$x+7]." = document.getElementById('close_".$info[$x+7]."');
-                                
-                                    btn".$info[$x+7].".onclick = function() {
-                                    modal".$info[$x+7].".style.display = 'block';
-
-                                }
-
-                                    span".$info[$x+7].".onclick = function() {
-                                        modal".$info[$x+7].".style.display = 'none';
-                                    }
-                                    window.onclick = function(event) {
-                                        if (event.target == modal".$info[$x+7].") {
-                                            modal".$info[$x+7].".style.display = 'none';
-                                        }
-                                    }
-
-
-                        </script>
-                
+                            <td style='border: none;'><button class='bouton_aff_modal' data-modal='modal_".$info[$x+7]."'>Modifier</button></td>
                         ";
                 $x=$x+8;
-            }
+            }       
         ?>
         </table>
         </div>
         
         </div>
         <?php include('vue/footer.php');?>
-
-<script>
-        var modal = document.getElementByClassName('modal');
-        var btn = document.getElementsByClassName('bouton_modal_modif');
-        var span = document.getElementsByClassName("close");
-        var x=0;
-        while(x<btn.length)
-            {
-                btn[x].onclick = function() {
-                modal[x].style.display = "block";
-
+        <script>
+            var boutonsAffModal=document.querySelectorAll(".bouton_aff_modal");
+            boutonsAffModal.forEach(function(bouton){
+                bouton.onclick=function(){
+                    var modal=bouton.getAttribute('data-modal');
+                    document.getElementById(modal).style.display='block';
                 }
-                
-                span[x].onclick = function() {
-                    modal[x].style.display = "none";
+            })
+            
+            var boutonsFermer=document.querySelectorAll(".close");
+            boutonsFermer.forEach(function(bouton){
+                bouton.onclick=function(){
+                    var modal=bouton.closest('.modal');
+                    modal.style.display='none';
                 }
-                window.onclick = function(event) {
-                    if (event.target == modal[x]) {
-                        modal[x].style.display = "none";
+            })
+            
+            window.onclick = function(event){
+                if(event.target.className== "modal")
+                    {
+                        event.target.style.display='none';
+                        
                     }
-                }
-                x++;
             }
-        
-
-</script>
+        </script>
     </body>
 </html>
