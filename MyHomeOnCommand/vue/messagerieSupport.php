5b96716552865a2122827d5efd15429bd99c87ee
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-01-16 12:05:04 +0100

    Update messagerieSupport.php

--- MyHomeOnCommand/vue/messagerieSupport.php
+++ MyHomeOnCommand/vue/messagerieSupport.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <link rel="stylesheet" href="css/styleGeneral.css" />
-        <link rel="stylesheet" href="css/styleSupportClient.css" />
+        <link rel="stylesheet" href="css/styleMessagerie.css" />
         <link rel="stylesheet" href="css/styleMenu.css" />
         <link rel="stylesheet" href="css/styleHeaderFooter.css" />
 
@@ -22,20 +22,33 @@
            include('vue/menuOperateur.php');
            ?>
 
-            <div id="CorpTexte">
+            <div id="corpsTexte">
 
                 <?php foreach($tabou as $truc): ?>
+
+                    <div class="boiteR">        <!-- Il faut bien prendre la class boiteR pour contenu_q puisque c'est le CSS utilisé pour le client,
+                                                on fait donc le symétrique en inversant comme ça -->
+                            <?php echo nl2br($truc['contenu_q']); ?>
+                        </div>
+
+                    <?php
+                    if ($truc['contenu_r'] !== NULL)    //N'afficher la réponse que si elle n'est pas vide
+                        {
+                    ?>
                     <div class="boiteQ">
                         <?php echo nl2br($truc['contenu_r']); ?>
                     </div>
-                    <div class="boiteR">
-                        <?php echo nl2br($truc['contenu_q']); ?>
-                    </div>
+                    <?php
+                        }
+                    ?>                        
+                    
                 <?php endforeach ?>
                 
                 <form method="post" id="formR">
                     <textarea name="textR" id="textR" rows="7" cols="50" placeholder="Saisissez-votre question ici"></textarea>
-                    <input name="Bouton_question" class="Bouton" type="submit" value="Envoyer">
+                    <input type="hidden" name="idCurrentClient" value=<?php echo $tabou[0]['id_utilisateur'];?>>
+                    <input type="hidden" name="bouton_lobby_repondre" value="bouton_lobby_repondre">
+                    <input name="bouton_repondre" class="Bouton" type="submit" value="Envoyer">
                 </form>
 
             </div>
