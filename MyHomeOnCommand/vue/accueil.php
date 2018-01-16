8c5326c77510515f33f27a5b97a82b9bd04ec171
nathanbienfait <nathan.bienfait78112@gmail.com>
2018-01-16 17:02:30 +0100

    Update accueil.php

--- MyHomeOnCommand/vue/accueil.php
+++ MyHomeOnCommand/vue/accueil.php
@@ -47,27 +47,27 @@
                     <p>
                         <label for="nom_inscription">Nom:</label>
                         </br>
-                        <input type="text" id="nom_inscription" name="nom_inscription" required/>
+                        <input type="text" id="nom_inscription" name="nom_inscription" onblur="verifNom(this)" required/>
                         </br>
                         </br>
                         <label for="prenom_inscription">Prénom:</label>
                         </br>
-                        <input type="text" id="prenom_inscription" name="prenom_inscription" required/>
+                        <input type="text" id="prenom_inscription" name="prenom_inscription" onblur="verifNom(this)" required/>
                         </br>
                         </br>
                         <label for="pseudo_inscription">Identifiant:</label>
                         </br>
-                        <input type="text" id="pseudo_inscription" name="pseudo_inscription" required/>
+                        <input type="text" id="pseudo_inscription" name="pseudo_inscription" onblur="verifNom(this)" required/>
                         </br>
                         </br>
                         <label for="mdp_inscription">Mot de passe:</label>
                         </br>
-                        <input type="password" id="mdp_inscription" name="mdp_inscription" required/>
+                        <input type="password" id="mdp_inscription" name="mdp_inscription" onblur="verifNom(this)" required/>
                         </br>
                         </br>
                         <label for="mdpconf_inscription">Confirmer le mot de passe:</label>
                         </br>
-                        <input type="password" id="mdpconf_inscription" name="mdpconf_inscription" required/>
+                        <input type="password" id="mdpconf_inscription" name="mdpconf_inscription" onblur="verifNom(this)" required/>
                         </br>
                         </br>
                         <input type="checkbox" name="checkconditions"required/>
@@ -89,9 +89,58 @@
 </div>
     <?php include('vue/footer.php');?>
     <script>
+        function surligne(champ, erreur)
+        {
+           if(erreur)
+              champ.style.backgroundColor = "#fba";
+           else
+              champ.style.backgroundColor = "";
+        }
+        function verifNom(champ)
+        {
+            if(champ.value.length < 2 || champ.value.length >30)
+           {
+              surligne(champ, true);
+              return false;
+           }
+           else
+           {
+              surligne(champ, false);
+              return true;
+
+           }
+
+        }
+        
+        function verifForm(f)
+        {
+            var nomOk = verifNom(f.nom_inscription);
+            var prenomOk = verifNom (f.prenom_inscription);
+            var identifiantOk = verifNom(f.pseudo_inscription);
+            var mdpOk = verifNom(f.mdp_inscription);
+            var mdpconfOk = verifNom(f.mdpconf_inscription);
+            if(nomOk && prenomOk && identifiantOk && mdpOk && mdpconfOk )
+            {
+                if(f.mdp_inscription==f.mdpconf_inscription)
+                    {
+                        return true;
+                    }
+                else
+                    {
+                        alert("Les mots de passes ne correspondent pas");
+                        return false;
+                    }
+            }
+            else
+            {
+                alert("Veuillez remplir correctement tous les champs");
+                return false;
+            }
+        }
+        
         document.inscription.onsubmit = function()
         {
-            return confirm('Confirmez votre inscriptions?');
+            return verifForm(this);
         }       
         var modal = document.getElementById('modal');
         var btn = document.getElementById("myBtn");
