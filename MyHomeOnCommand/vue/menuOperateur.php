6d771b86e8110a7a3a995d7a716d4b1561165541
ElodieOkami <33449924+ElodieOkami@users.noreply.github.com>
2018-01-18 11:46:46 +0100

    Update menuOperateur.php

--- MyHomeOnCommand/vue/menuOperateur.php
+++ MyHomeOnCommand/vue/menuOperateur.php
@@ -1,10 +1,8 @@
 <div class="menu">
     	<h1 id="h1Menu">Menu</h1>
         <ul id="ulmenu">
-            <a href="index.php?page=supportOperateur" class="listeMenu">Support<li></li></a>
             <a href="index.php?page=messagerieSupport" class="listeMenu">Messagerie<li class></li></a>
-  
-            
+            <a href="index.php?page=supportOperateur" class="listeMenu">Support<li></li></a>  
         </ul>
         <a href="index.php?page=accueil" class="Mdeconnexion">Se deconnecter</a>
     </div>
