f70a7d7f40317c4f3698b8a1f2d3b58034e4842c
Aenerya <33449933+Aenerya@users.noreply.github.com>
2018-01-19 12:06:57 +0100

    Update modeleAdmin.php

--- MyHomeOnCommand/modele/modeleAdmin.php
+++ MyHomeOnCommand/modele/modeleAdmin.php
@@ -33,4 +33,34 @@ class admin extends Connection
         ));
 
     }
+    
+ //Partie consommation
+    public function getconsommationtemperatureadmin()
+    {
+        $db=$this->dbConnect();
+        $reponse = $db-> query('SELECT donnees_equipement.id_equipement, donnees_equipement.temps, donnees_equipement.date_utilisation, equipement.id_equipement, donnees_equipement.valeur FROM donnees_equipement INNER JOIN equipement ON equipement.id_equipement = donnees_equipement.id_equipement WHERE equipement.id_type_equipement=1');
+        return $reponse;        
+    }
+
+    public function getconsommationhumiditeadmin()
+    {
+        $db=$this->dbConnect();
+        $reponse = $db-> query('SELECT donnees_equipement.id_equipement, donnees_equipement.date_utilisation, equipement.id_equipement, donnees_equipement.valeur FROM donnees_equipement INNER JOIN equipement ON equipement.id_equipement = donnees_equipement.id_equipement WHERE equipement.id_type_equipement=2');
+        return $reponse;       
+    }
+
+
+    public function getconsommationlumiereadmin()
+    {
+        $db=$this->dbConnect();
+        $reponse = $db-> query('SELECT donnees_equipement.id_equipement, donnees_equipement.temps, donnees_equipement.date_utilisation, equipement.id_equipement, relation_logement_utilisateur.id_utilisateur
+            FROM donnees_equipement 
+                INNER JOIN equipement ON equipement.id_equipement = donnees_equipement.id_equipement 
+                INNER JOIN relation_piece_cemac ON relation_piece_cemac.id_cemac = equipement.id_cemac 
+                INNER JOIN piece ON piece.id_piece = relation_piece_cemac.id_piece 
+                INNER JOIN logement ON logement.id_logement = piece.id_logement 
+                INNER JOIN relation_logement_utilisateur ON relation_logement_utilisateur.id_logement = logement.id_logement  
+            WHERE donnees_equipement.valeur=1 and equipement.id_type_equipement=4');
+        return $reponse;      
+    }
 }
