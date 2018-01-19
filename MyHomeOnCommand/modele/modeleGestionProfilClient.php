0015b157692e1dab2f0a0d592b97cfa4c1549569
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-01-19 12:05:00 +0100

    Update modeleGestionProfilClient.php

--- MyHomeOnCommand/modele/modeleGestionProfilClient.php
+++ MyHomeOnCommand/modele/modeleGestionProfilClient.php
@@ -38,12 +38,30 @@ class gestionProfilClient
 	public function clientModifUtil($login,$idUtil)
 	{
 		$db=$this->dbConnect();
-		$req = $db->prepare('UPDATE utilisateur SET login =:login, WHERE id_utilisateur = :id');
+		$req = $db->prepare('UPDATE utilisateur SET login =:login WHERE id_utilisateur = :id');
         $req->execute(array(
             'login' => $login,
             'id' => $idUtil
         ));
 	}
+
+    public function getMdpUtilisateur($idUtil)
+    {
+        $db=$this->dbConnect();
+        $req = $db->prepare('SELECT password FROM utilisateur WHERE id_utilisateur = :id');
+        $req->execute(array('id' => $idUtil));
+        return $req;
+    }
+
+    public function modifMdp($criptedMdp, $idUtil)
+    {
+        $db=$this->dbConnect();
+        $req = $db->prepare('UPDATE utilisateur SET password = :mdp WHERE id_utilisateur = :id');
+        $req->execute(array(
+            'mdp' => $criptedMdp,
+            'id' => $idUtil
+        ));
+    }
 	
 }
 
