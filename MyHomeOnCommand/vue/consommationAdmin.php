3674b35aa9b2932989c8e5c16628582a2f15d983
Aenerya <33449933+Aenerya@users.noreply.github.com>
2018-01-19 12:03:21 +0100

    Update consommationAdmin.php

--- MyHomeOnCommand/vue/consommationAdmin.php
+++ MyHomeOnCommand/vue/consommationAdmin.php
@@ -11,26 +11,121 @@
     </head>
 
     <body>
-    <?php include("header.php"); ?>
+        <?php include("header.php"); 
+        ?>
 
-    <div id="corps">
+        <div id="corps">
 
-        <?php include("menuAdmin.php"); ?>
+            <?php include("menuAdmin.php");  
+            ?>
 
-        <ul id="liste_graph">
-            <li id="lumieres">Temps moyen d'utilisation des lumières </br>
-                <img id="imagegraphe2" src="images/exgraphe2.png" alt="Exemple de graphe" /></li>
-            <li id="electricite">Consommation d'électricité </br>
-                <img id="imagegraphe1" src="images/exgraphe1.png" alt="Exemple de graphe" /></li> 
-            <li id="eau">Consommation d'eau </br>
-                <img id="imagegraphe1" src="images/exgraphe1.png" alt="Exemple de graphe" /></li>
-            <li id="gaz">Consommation de gaz </br>
-                <img id="imagegraphe2" src="images/exgraphe2.png" alt="Exemple de graphe" /></li>
-        </ul>
 
-    </div>
+            <div id='graphes'>
+                <p>Bienvenue sur la page consommation. Ici, vous pouvez accèder aux statistiques de nos utilisateurs.</p>
 
-    <?php include("footer.php"); ?>
+                <div id='lumière'>
+                    <?php 
+    //Partie lumière
+                        $arrlum = adminGrapheConsommationlumiere();
+                    ?>
+           
+
+                    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                    <script type="text/javascript">
+                        var tablum=<?php echo json_encode($arrlum);?>;
+
+                        google.charts.load("current", {packages:['corechart']});
+                        google.charts.setOnLoadCallback(drawChart);
+                        function drawChart() {
+                            var data = google.visualization.arrayToDataTable(tablum);
+
+                            var view = new google.visualization.DataView(data);
+                            view.setColumns([0, 1,
+                                { calc: "stringify",
+                                sourceColumn: 1,
+                                type: "string",
+                                role: "annotation" }]);
+
+                            var options = {
+                                title: "Temps d'utilisation des lumières en heure par jour",
+                                width: 700,
+                                height: 400,
+                                bar: {groupWidth: "95%"},
+                                legend: { position: "none" },
+                            };
+                        var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+                        chart.draw(view, options);
+                        }
+                    </script>
+                    <div id="columnchart_values" style="width: 900px; height: 300px;"></div>
+                </div>
+
+                <div id='humidité'>
+                    <?php 
+    //Partie humidité
+                        $arrhum = adminGrapheConsommationhumidite();
+                    ?>
+
+                    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                    <script type="text/javascript">
+                    var tabhum=<?php echo json_encode($arrhum);?>;
+                    google.charts.load('current', {'packages':['corechart']});
+                    google.charts.setOnLoadCallback(drawChart);
+
+                    function drawChart() {
+                        var data = google.visualization.arrayToDataTable(tabhum);
+
+                        var options = {
+                            title: 'Humidité relative en pourcentage par jour',
+                            curveType: 'function',
+                            width: 700,
+                            height: 400,
+                        };
+
+                        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+                        chart.draw(data, options);
+                    }
+                    </script>
+                    <div id="curve_chart" style="width: 900px; height: 500px"></div>
+
+                </div>
+
+                <div id='température'> <br/>
+                    <?php 
+    //Partie température
+                        $arrtem = adminGrapheConsommationtemperature();
+                    ?>
+
+                    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                    <script type="text/javascript">
+                    var tabtem=<?php echo json_encode($arrtem);?>;
+                    google.charts.load('current', {'packages':['corechart']});
+                    google.charts.setOnLoadCallback(drawChart);
+
+                    function drawChart() {
+                        var data = google.visualization.arrayToDataTable(tabtem);
+
+                        var options = {
+                            title: 'Température en degré celsius par jour',
+                            width: 700,
+                            height: 400,
+                            hAxis: {titleTextStyle: {color: '#333'}},
+                            vAxis: {minValue: 0}
+                        };
+
+                        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+                        chart.draw(data, options);
+                    }
+                    </script>
+                    <div id="chart_div" style="width: 100%; height: 500px;"></div>
+                </div>
+
+     
+            </div>
+        </div>
+
+ <?php include("footer.php"); ?>
 
 </body>
 </html>
