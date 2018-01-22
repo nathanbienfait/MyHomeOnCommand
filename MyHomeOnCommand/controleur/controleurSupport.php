c0f3d321dfb0a92f62897db3768a94c9680f9b21
AlexisDelorme <33450034+AlexisDelorme@users.noreply.github.com>
2018-01-22 11:21:22 +0100

    Update controleurSupport.php

--- MyHomeOnCommand/controleur/controleurSupport.php
+++ MyHomeOnCommand/controleur/controleurSupport.php
@@ -2,24 +2,24 @@
 function tableauqr()
 {
     $clientqr=new QuestionReponse; /* Permet d'ouvrir l'objet contenant les fonctions correspondant au service question/réponse */
-    $qr=$clientqr->recupereqr()->fetchAll();
+    $qr=$clientqr->recupereqr()->fetchAll(); //fait appel à la fonction contenant le SQL pour récupérer les questions/réponses et leur id
     $tableauqr = $qr;
-    return $tableauqr;
+    return $tableauqr; //renvoie un fetch avec toutes les questions/réponses
 }
 
 function supprimerqr($id)
 {
 
-    $clientqr=new QuestionReponse;
-    $supprimer=$clientqr->supprimer($id); 
+    $clientqr=new QuestionReponse; 
+    $supprimer=$clientqr->supprimer($id);  //fait appel à la fonction SQL supprimant les questions/réponses en fonction de leur id
 }
 function modifqr($idqr,$contenur,$contenuq)
 {
     $qr = new QuestionReponse;
-    $qr->modifier($idqr,$contenur,$contenuq);
+    $qr->modifier($idqr,$contenur,$contenuq); //fait appel à la fonction SQL modifiant les questions/réponses en fonction de leur id
 }
 function ajouterqr($texteQ,$texteR,$dateQ,$dateR)
 {
     $aj = new QuestionReponse;
-    $aj->ajouter($texteQ,$texteR,$dateQ,$dateR);
+    $aj->ajouter($texteQ,$texteR,$dateQ,$dateR); //fait appel à la fonction SQL ajoutant les questions/réponses en fonction de leur id
 }
