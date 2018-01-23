8a7f201b2ac9db50b450899cc5e4dc04bac80e61
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-01-23 21:24:29 +0100

    Update controleurMessagerieSupport.php

--- MyHomeOnCommand/controleur/controleurMessagerieSupport.php
+++ MyHomeOnCommand/controleur/controleurMessagerieSupport.php
@@ -2,9 +2,9 @@
 
 function visuLobbyMessagerieSupport()
 {
-	$utilisateur=new messagerieSupport;
+	$utilisateur=new messagerieSupport;		//On se prépare a utiliser des requêtes SQL de la classe messagerieSupport
 
-	$tab=$utilisateur->recupNewQuestions()->fetchAll();
+	$tab=$utilisateur->recupNewQuestions()->fetchAll();		//On fetch toutes les questions avec pour réponse une réponse automatique
 
 	return $tab;
 }
@@ -13,7 +13,7 @@ function visuMessagerieSupport($idClient)
 {
 	$utilisateur=new messagerieSupport;
 
-	$tabou=$utilisateur->getMessagesClient($idClient)->fetchAll();
+	$tabou=$utilisateur->getMessagesClient($idClient)->fetchAll();		//On fetch tous les messages d'un client en particulier grace a son id
 
 	return $tabou;
 }
@@ -22,10 +22,9 @@ function postReponseSupport($reponse, $idClient)
 {
 	$utilisateur=new messagerieSupport;
 
-	$idQR = $utilisateur->recupIdQr($idClient)->fetch();
+	$idQR = $utilisateur->recupIdQr($idClient)->fetch();		//On réupère l'id_qr de la question à laquelle on va répondre
 	
-	$utilisateur->postReponse($reponse, $idQR[0]);
+	$utilisateur->postReponse($reponse, $idQR[0]);				//On répond à la question grâce à son id
 }
-
 
 ?>
