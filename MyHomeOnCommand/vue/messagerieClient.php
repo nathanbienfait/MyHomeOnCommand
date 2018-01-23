bf8f7494bc4fc35f2e9af74fa6bc61579e733072
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-01-23 21:26:17 +0100

    Update messagerieClient.php

--- MyHomeOnCommand/vue/messagerieClient.php
+++ MyHomeOnCommand/vue/messagerieClient.php
@@ -24,17 +24,31 @@
 
             <div id="corpsTexte">
 
-                <?php foreach($tab as $truc): ?>
-                    <div class="boiteQ">
-                        <?php echo nl2br($truc['contenu_q']); ?>
-                    </div>
+                <?php 
+                    if ($premierMessage == 1)    /*Si aucune question n'a encore été posée (si $tab est vide), $premierMessage a la valeur de 1 et on affiche
+                                                donc un message expliquant que faire ici */
+                    {
+                    ?>
+                        <div class="boiteQ">
+                            <p> Vous n'avez pas encore posé de question. Vous pouvez poser votre première question juste en dessous. Pensez toutefois à vérifier que votre question n'a pas déjà été posée sur notre page FAQ. </p>
+                        </div>
+                    <?php
+                    }
+                    ?>
+
+                <?php foreach($tab as $truc):  //Pour chaque élément (contenu_q et contenu_r) fetched avec visuMessagerieClient dans controleurMessagerieClient ?>
+
+                        <div class="boiteQ">
+                            <?php echo nl2br($truc['contenu_q']); //On affiche une question contenu_q ?>   
+                        </div>
+
 
                     <?php
                     if ($truc['contenu_r'] !== NULL)    //N'afficher la réponse que si elle n'est pas vide
                         {
                     ?>
                         <div class="boiteR">
-                            <?php echo nl2br($truc['contenu_r']); ?>
+                            <?php echo nl2br($truc['contenu_r']); //On affiche la réponse contenu_r correspondante à sa contenu_q ?>
                         </div>
                     <?php
                         }
@@ -42,8 +56,8 @@
                 <?php endforeach ?>
                 
                 <form method="post" id="formQ">
-                    <textarea name="textQ" id="textQ" rows="7" cols="50" placeholder="Saisissez-votre question ici"></textarea>
-                    <input name="Bouton_question" class="Bouton" type="submit" value="Envoyer">
+                    <textarea name="textQ" id="textQ" rows="7" cols="50" placeholder="Saisissez-votre question ici"></textarea> 
+                    <input name="Bouton_question" class="Bouton" type="submit" value="Envoyer">     <!-- On envoie à controleurRoute le message du form --> 
                 </form>
 
             </div>
