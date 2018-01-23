77c3fe7da12282323532876d5d876eba91959071
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-01-23 21:24:14 +0100

    Update controleurMessagerieClient.php

--- MyHomeOnCommand/controleur/controleurMessagerieClient.php
+++ MyHomeOnCommand/controleur/controleurMessagerieClient.php
@@ -3,13 +3,12 @@
 function visuMessagerieClient()
 {
 
-$idClient=$_SESSION['id'];
-$utilisateur=new messagerieClient;
+$idClient=$_SESSION['id'];									//On crée la variable $idClient contenant l'id du client actuel
+$utilisateur=new messagerieClient;							//On se prépare a utiliser des requêtes SQL de la classe messagerieClient 
+															
+$tab=$utilisateur->getMessages($idClient)->fetchAll();		//On fetch toutes les données procurées par la requête SQL getMessages
 
-$tab=$utilisateur->getMessages($idClient)->fetchAll();
-//$nbrLignes=$utilisateur->getNombreQR($idClient);
-
-return $tab;
+return $tab;												//On retourne ces valeurs dans le controleurRoute
 }
 
 function postQuestionClient($question)
@@ -17,24 +16,24 @@ function postQuestionClient($question)
 	$idClient=$_SESSION['id'];
 	$utilisateur=new messagerieClient;
 
-	$utilisateur->postQuestion($question);
-	$utilisateur->insertIdRelation($idClient);
+	$utilisateur->postQuestion($question);					//On appelle la requête postQuestion avec comme objet $question fournie par le controleurRoute
+	$utilisateur->insertIdRelation($idClient);				//On appelle la requête insertIdRelation
 }
 
-function eviteAttenteSupportDoublon()
+function eviteAttenteSupportDoublon()						//Fonction qui sert à éviter que le message "un opérateur va répondre" apparaisse plusieurs fois
 {
 	$idClient=$_SESSION['id'];
 	$utilisateur=new messagerieClient;
 
-	$tab = $utilisateur->verifAttenteSupportDoublon($idClient)->fetchAll();
+	$tab = $utilisateur->verifAttenteSupportDoublon($idClient)->fetchAll(); 	//On récupère tous les id_qr des questions avec une réponse automatique
 
 		foreach($tab as $item):
-			$utilisateur->supprAttenteSupportDoublon($item['id_qr']);
-			$lastId=$item['id_qr'];
+			$utilisateur->supprAttenteSupportDoublon($item['id_qr']);			//On supprime toutes les réponses automatiques
 		endforeach;
 
-	$utilisateur->reecrireDerniereReponse();
-	
+	$utilisateur->reecrireDerniereReponse();				//On ré-écrit la réponse automatique pour la dernière question posée
+												/* Il y a forcément un moyen plus efficace pour ne supprimer toutes les réponses automatiques sauf la dernière.
+												Toutefois ce moyen n'a pas été trouvé et la méthode actuel fonctionne très bien */
 }
 
 ?>
