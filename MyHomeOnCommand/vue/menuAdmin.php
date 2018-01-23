62718bae735936177045b4318ecccc87a499d25a
ElodieOkami <33449924+ElodieOkami@users.noreply.github.com>
2018-01-23 11:53:00 +0100

    Update menuAdmin.php

--- MyHomeOnCommand/vue/menuAdmin.php
+++ MyHomeOnCommand/vue/menuAdmin.php
@@ -4,7 +4,7 @@
             <a href="index.php?page=adminPanneauClient" class="listeMenu">Données Client<li></li></a>
             <a href="index.php?page=supportAdmin" class="listeMenu">Support<li></li></a>
             <a href="index.php?page=consommationAdmin" class="listeMenu">Consommation<li></li></a>
-            <a href="index.php?page=modification" class="listeMenu">Modification<li></li></a>
+            <a href="index.php?page=modification" class="listeMenu">Gestion du site<li></li></a>
         </ul>
         <a href="index.php?page=accueil" class="Mdeconnexion">Se deconnecter</a>
     </div>
