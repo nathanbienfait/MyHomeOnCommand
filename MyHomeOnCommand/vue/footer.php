05d504d37492f6f7b06806bb91ac801ee6f73e18
Aenerya <33449933+Aenerya@users.noreply.github.com>
2018-01-23 14:17:08 +0100

    Update footer.php

--- MyHomeOnCommand/vue/footer.php
+++ MyHomeOnCommand/vue/footer.php
@@ -1,1 +1,1 @@
-<div id="bas"><a href="index.php?page=conditions">conditions d'utilisation</a>&nbsp&nbsp&nbsp&nbsp<a href="index.php?page=mentionsLegales">mentions légales</a></div>
+<div id="bas"><a id='liensfooter' href="index.php?page=conditions">conditions d'utilisation</a>&nbsp&nbsp&nbsp&nbsp<a id='liensfooter' href="index.php?page=mentionsLegales">mentions légales</a></div>
