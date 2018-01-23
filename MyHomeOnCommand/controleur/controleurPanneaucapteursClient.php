b55ece306624e19221c41ba652af10e07b9909c7
RemiBiolley <33449916+RemiBiolley@users.noreply.github.com>
2018-01-23 23:18:05 +0100

    Update controleurPanneaucapteursClient.php

--- MyHomeOnCommand/controleur/controleurPanneaucapteursClient.php
+++ MyHomeOnCommand/controleur/controleurPanneaucapteursClient.php
@@ -1,27 +1,31 @@
 <?php
 
 
-if (!isset($_POST['tri']) || empty($_POST['tri']))
+
+
+if (!isset($_POST['tri']) || empty($_POST['tri'])) /* lorsqu'on arrive pour la premiere fois sur la page : */
 {
 	$fonction = 'piece';
 }
-else
+else /* lorsqu'on a choisi un type de tri grace au formulaire de la page panneaucontroleClient(Trie) */
 {
 	$fonction=$_POST['tri'];
 }
 
-if(isset($_POST['id_equipement']) AND isset($_POST['valeur_cible']))
+$panneau=new panneauControle; /* permettra d'utiliser les fonctions de la classe "panneauControle" definie dans le modelePanneaucapteursClient */
+
+if(isset($_POST['id_equipement']) AND isset($_POST['valeur_cible'])) /* si on a choisi une nouvelle valeur cible pour nos equipements, l'enregistre dans la bdd */
 {
-	MajValeurCible(htmlspecialchars($_POST['id_equipement']), htmlspecialchars($_POST['valeur_cible']));
+	$panneau->MajValeurCible(htmlspecialchars($_POST['id_equipement']), htmlspecialchars($_POST['valeur_cible']));
 }
 
-switch ($fonction)
+switch ($fonction) /* selon le type de tri choisi, appelle la page correspondante */
 {
 	case 'piece' :
 
 		$vue='panneaucontroleClient';
 		$title='Vos capteurs';
-		$id_logements=Obtenir_id_logements(htmlspecialchars($_SESSION['id']));
+		$id_logements=$panneau->Obtenir_id_logements(htmlspecialchars($_SESSION['id']));
 		break;
 
 	case 'type_parametre' :
@@ -29,7 +33,7 @@ switch ($fonction)
 		$title='Vos capteurs';
 		$compteType=0;
 		$compteLogement=0;
-		$idtypesEquipement=Obtenir_tous_id_type_equipement();
+		$idtypesEquipement=$panneau->Obtenir_tous_id_type_equipement();
 		break;
 }
 
