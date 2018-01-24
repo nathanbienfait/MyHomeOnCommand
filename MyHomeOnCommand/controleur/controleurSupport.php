1a8e1f9edbb7d635f4138d085cb6e45340d044e1
AlexisDelorme <33450034+AlexisDelorme@users.noreply.github.com>
2018-01-24 10:36:05 +0100

    Update controleurSupport.php

--- MyHomeOnCommand/controleur/controleurSupport.php
+++ MyHomeOnCommand/controleur/controleurSupport.php
@@ -1,5 +1,5 @@
 <?php
-function tableauqr()
+function tableauqr() //Fonction permettant d'afficher par la suite les questions/réponses
 {
     $clientqr=new QuestionReponse; /* Permet d'ouvrir l'objet contenant les fonctions correspondant au service question/réponse */
     $qr=$clientqr->recupereqr()->fetchAll(); //fait appel à la fonction contenant le SQL pour récupérer les questions/réponses et leur id
@@ -7,18 +7,18 @@ function tableauqr()
     return $tableauqr; //renvoie un fetch avec toutes les questions/réponses
 }
 
-function supprimerqr($id)
+function supprimerqr($id) //Fonction qui nous permet de supprimer une question/reponse en fonction de l'id
 {
 
     $clientqr=new QuestionReponse; 
     $supprimer=$clientqr->supprimer($id);  //fait appel à la fonction SQL supprimant les questions/réponses en fonction de leur id
 }
-function modifqr($idqr,$contenur,$contenuq)
+function modifqr($idqr,$contenur,$contenuq) //Fonction qui nous permet de modifier une question/reponse en fonction de l'id
 {
     $qr = new QuestionReponse;
     $qr->modifier($idqr,$contenur,$contenuq); //fait appel à la fonction SQL modifiant les questions/réponses en fonction de leur id
 }
-function ajouterqr($texteQ,$texteR,$dateQ,$dateR)
+function ajouterqr($texteQ,$texteR,$dateQ,$dateR) //Fonction qui nous permet d'ajouter une question/reponse
 {
     $aj = new QuestionReponse;
     $aj->ajouter($texteQ,$texteR,$dateQ,$dateR); //fait appel à la fonction SQL ajoutant les questions/réponses en fonction de leur id
